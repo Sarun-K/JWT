9b618d2a0d4f87ca54611a0c535028a3a20c1f42
SARUN <>
2019-09-15 09:58:49 +0700

    npm install body-parser --save

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+var mongoose = require("mongoose");
+
+const mongoURI = 'mongodb://localhost:27017/JWT-NodeJs';
+var mongoose = require("mongoose");
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
