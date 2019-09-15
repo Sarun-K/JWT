094951ebbba7b1f99b60ac443d70644fc77ab46f
SARUN <>
2019-09-15 15:24:38 +0700

    npm install --save lodash

--- server.js
+++ server.js
@@ -5,17 +5,26 @@ const logger = require('morgan');
 
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var router = express.Router(); 
 
 const mongoURI = 'mongodb://localhost:27017/JWT-NodeJs';
 var mongoose = require("mongoose");
 
-
-
+app.set('secretKey', 'jwt-nodejs-test'); // jwt secret token
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb'  }));
 app.use(bodyParser.json({ limit: '10mb' }));
+app.use('/api', router);
 
-var router = express.Router(); 
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+///////////////////////////////////////////////
+///// API
+///////////////////////////////////////////////
