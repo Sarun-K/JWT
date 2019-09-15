d97fdcaf36e53fb8306ffdb3f41c1e49b788c29b
SARUN <>
2019-09-15 10:19:51 +0700

    npm install --save cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const logger = require('morgan');
 
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
 
 const mongoURI = 'mongodb://localhost:27017/JWT-NodeJs';
 var mongoose = require("mongoose");
+
+
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb'  }));
+app.use(bodyParser.json({ limit: '10mb' }));
+
+var router = express.Router(); 
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
